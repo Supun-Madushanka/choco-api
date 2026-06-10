537488e985a0837e0942ed47bc9b13657ad0969f
Supun Madushanka <supunmadushanka2582000@gmail.com>
2026-06-10 12:28:30 +0530

    Initialize chocolate factory API project with Spring Boot and Maven setup

---
+++ src/main/java/com/ceylonechocolate/chocolate_factory_api/ChocolateFactoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ceylonechocolate.chocolate_factory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateFactoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateFactoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceylonechocolate/chocolate_factory_api/ChocolateFactoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceylonechocolate.chocolate_factory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateFactoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
